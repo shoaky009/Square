1088280fe2a1274b46b71173a31af22751ed0b0b
wuldas <wuldas@qq.com>
2026-07-26 17:25:45 +0800

    chore: 完善轻量示例图片支持

--- samples/Square.Sample.HelloWorld/Program.cs
+++ samples/Square.Sample.HelloWorld/Program.cs
@@ -1,5 +1,8 @@
 using Square.Controls;
 using Square.Hosting;
+using Square.Images;
+
+ImageSourceRegistration.RegisterDefaults();
 
 var window = new AppWindow("Hello World", 480, 320);
 window.Load(new Text("Hello World"));
